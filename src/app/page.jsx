96794713ac471e1a78741199f5998582e149d1e7
yube01 <yubraja008@gmail.com>
2024-03-26 16:59:06 +0545

    homepage design complete

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
-// import styles from "./page.module.css";
+
+import Featured from "@/components/featured/Featured";
+import styles from "./homePage.module.css";
+import CategoryList from "@/components/categoryList/CategoryList";
+import Menu from "@/components/menu/Menu";
+import CardList from "@/components/cardList/CardList";
 
 export default function Home() {
   return (
-  <div>
-
-  </div>
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList page={"test"}/>
+        <Menu />
+      </div>
+    </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
-// import AuthLinks from "../authLinks/AuthLinks";
-// import ThemeToggle from "../themeToggle/ThemeToggle";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
   return (
@@ -16,11 +16,11 @@ const Navbar = () => {
       </div>
       <div className={styles.logo}>Yube</div>
       <div className={styles.links}>
-        {/* <ThemeToggle /> */}
+        <ThemeToggle />
         <Link href="/" className={styles.link}>Homepage</Link>
         <Link href="/" className={styles.link}>Contact</Link>
         <Link href="/" className={styles.link}>About</Link>
-        {/* <AuthLinks /> */}
+        <AuthLinks />
       </div>
     </div>
   );
