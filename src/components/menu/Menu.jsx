a40085369342cec425e092f6c15ad68054df769f
yube01 <yubraja008@gmail.com>
2024-03-27 13:33:18 +0545

    blog page design complete

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import styles from "./menu.module.css";
-import Link from "next/link";
-import Image from "next/image";
-// import MenuPosts from "../menuPosts/MenuPosts";
-// import MenuCategories from "../menuCategories/MenuCategories";
+
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCatagories/MenuCategories";
 
 const Menu = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.subtitle}>{"What's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
-      {/* <MenuPosts withImage={false} /> */}
+      <MenuPosts withImage={false} />
       <h2 className={styles.subtitle}>Discover by topic</h2>
       <h1 className={styles.title}>Categories</h1>
-      {/* <MenuCategories /> */}
+      <MenuCategories />
       <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>Editors Pick</h1>
-      {/* <MenuPosts withImage={true} /> */}
+      <MenuPosts withImage={true} />
     </div>
   );
 };
