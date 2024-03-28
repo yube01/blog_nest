d5379d46c439b9e1b2c80af38baf2d104398d0ca
yube01 <yubraja008@gmail.com>
2024-03-28 16:55:37 +0545

    login module

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Navbar from "@/components/navbar/Navbar"
 import Footer from "@/components/footer/Footer"
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
+import AuthProvider from "@/providers/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,7 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ThemeContextProvider>
+        <AuthProvider>
+        <ThemeContextProvider>
             <ThemeProvider>
               <div className="container">
                 <div className="wrapper">
@@ -26,6 +28,7 @@ export default function RootLayout({ children }) {
               </div>
             </ThemeProvider>
           </ThemeContextProvider>
+        </AuthProvider>
       </body>
     </html>
   );
