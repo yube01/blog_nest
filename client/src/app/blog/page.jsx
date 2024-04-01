7f9eea33e7075a4235839b949544f95752555e51
yube01 <yubraja008@gmail.com>
2024-04-01 15:53:15 +0545

    bug fix

--- client/src/app/blog/page.jsx
+++ client/src/app/blog/page.jsx
@@ -1,4 +1,4 @@
-import CardList from "@/components/cardList/CardList";
+
 import styles from "./blogPage.module.css";
 import Menu from "@/components/Menu/Menu";
 import { recent } from "../../../url";
